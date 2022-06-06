014f34e8f7dd9d980956fb2d7948836bb1d39a8e
Gabriel Saillard <gabriel.saillard@alan.eu>
2022-06-06 16:59:52 +0200

    nvim: fix python lsp servers not autostarting

--- .config/nvim/lua/lsp-servers.lua
+++ .config/nvim/lua/lsp-servers.lua
@@ -14,7 +14,34 @@ lsp.cssls.setup({ capabilities = capabilities }) -- npm i -g vscode-langservers-
 -- lsp.eslint.setup({ capabilities = capabilities }) -- ↑↑↑
 lsp.html.setup({ capabilities = capabilities }) -- ↑↑↑
 lsp.jsonls.setup({ capabilities = capabilities }) -- ↑↑↑
--- lsp.denols.setup({ capabilities = capabilities }) -- brew install deno
+lsp.tsserver.setup({ autostart = true, capabilities = capabilities }) -- npm i -g typescript typescript-language-server
+lsp.vimls.setup({ capabilities = capabilities }) -- npm i -g vim-language-server
+lsp.yamlls.setup({ capabilities = capabilities }) -- npm i -g yaml-language-server
+lsp.pylsp.setup({ -- pip install -U 'python-lsp-server[all]' preload pyls-flake8 python-lsp-black pyls-mypy pyls-isort
+	capabilities = capabilities,
+	settings = {
+		pylsp = {
+			plugins = {
+				pycodestyle = {enabled = false},
+				black = {
+					enabled = true,
+					cache_config = true,
+				},
+				flake8 = {
+					enabled = true,
+				},
+				mypy = {
+					enabled = true,
+					live_mode = true,
+					dmypy = true,
+				},
+				isort = {
+					enabled = true,
+				},
+			}
+		}
+	},
+})
 local lua_runtime_path = vim.split(package.path, ';')
 table.insert(lua_runtime_path, "lua/?.lua")
 table.insert(lua_runtime_path, "lua/?/init.lua")
@@ -37,6 +64,3 @@ lsp.sumneko_lua.setup({  -- brew install lua-language-server
 		},
 	}
 })
-lsp.tsserver.setup({ capabilities = capabilities }) -- npm i -g typescript typescript-language-server
-lsp.vimls.setup({ capabilities = capabilities }) -- npm i -g vim-language-server
-lsp.yamlls.setup({ capabilities = capabilities }) -- npm i -g yaml-language-server
