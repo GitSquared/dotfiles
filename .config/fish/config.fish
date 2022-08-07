648ed84fd065cc800c7ed29a79410f6a99fa33a6
Gabriel Saillard <gabriel@saillard.dev>
2022-08-07 16:50:22 +0200

    add pinentry-touchid support

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,6 +12,7 @@ nvm use default --silent
 
 # Homebrew updates disabled here as they need to be one with the main user account
 alias full_upgrade='npm update -g;pip list --user | tail -n +3 | awk "{print $1}" | xargs pip install --user -U; fish_update_completions; ncu -g; fisher update; tldr --update;nvim --headless -c "TSUpdateSync" -c "autocmd User PackerComplete quitall" -c "PackerSync"'
+alias pinentry='pinentry-mac'
 alias weather='curl wttr.in'
 
 alias ls='exa -l --git --group-directories-first --time-style=iso --icons'
