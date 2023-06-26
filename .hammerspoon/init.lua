997098be33000ae6e0b8d7167153fd8aa417520e
Gabriel Saillard <gabriel.saillard@alan.eu>
2023-06-26 12:13:14 +0200

    add stackline config, rework yabai config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,2 +1,9 @@
+hs.ipc.cliInstall("/opt/homebrew")
+
 stackline = require("stackline")
 stackline:init()
+
+stackline.config:set("paths.yabai", "/opt/homebrew/bin/yabai")
+stackline.config:set("appearance.showIcons", false)
+stackline.config:set("appearance.offset.x", 3)
+stackline.config:set("appearance.offset.y", 10)
