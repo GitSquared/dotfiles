c5736368c9354a71c62ed055b94caaceb44d46d3
Gabriel Saillard <gabriel.saillard@alan.eu>
2023-08-02 19:29:10 +0200

    nvim: show symbol_text for autocompletion suggestions

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -141,7 +141,7 @@ return require('packer').startup(function(use)
 				},
 				formatting = {
 					format = lspkind.cmp_format({
-						mode = 'symbol',
+						mode = 'symbol_text',
 						maxwidth = 50,
 						ellipsis_char = '…',
 					})
