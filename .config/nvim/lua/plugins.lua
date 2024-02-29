74bf4e07d2c81fcac8c71414fbb24f24f8d6c9b8
Gabriel Saillard <gabriel@saillard.dev>
2024-02-29 20:31:48 +0100

    nvim: show signs instead of letters in diagnostics status column

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -624,10 +624,22 @@ return require('lazy').setup({
 	{
 		'folke/trouble.nvim', -- list lsp diagnostics
 		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		opts = {
+			mode = "document_diagnostics",
+		},
 		config = function()
-			require('trouble').setup({
-				mode = "document_diagnostics"
-			})
+			-- redefine the signs used by nvim's LSP to show diagnotics in the statuscolumn
+			local signs = {
+				Error = "",
+				Warn = "",
+				Hint = "",
+				Info = "",
+			}
+
+			for type, icon in pairs(signs) do
+				local hl = "DiagnosticSign" .. type
+				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+			end
 		end
 	},
 
