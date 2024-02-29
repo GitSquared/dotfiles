25783ba36aab71f9c2fcf8424358489bf23ae412
Gabriel Saillard <gabriel@saillard.dev>
2024-02-29 20:13:16 +0100

    nvim: modern folds support through nvim-ufo

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -733,5 +733,75 @@ return require('lazy').setup({
 		end
 	},
 
+	{
+		'kevinhwang91/nvim-ufo', -- modern folding mechanisms
+		dependencies = { 'kevinhwang91/promise-async', 'nvim-treesitter/nvim-treesitter' },
+		config = function()
+			vim.o.foldcolumn = '1'
+			vim.o.foldlevel = 99
+			vim.o.foldlevelstart = 99
+			vim.o.foldenable = true
+			vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+
+			-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+			vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+			vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
+			-- display count of folded lines in virtual text
+			local handler = function(virtText, lnum, endLnum, width, truncate)
+				 local newVirtText = {}
+				 local suffix = (' 󰁂 %d lines folded '):format(endLnum - lnum)
+				 local sufWidth = vim.fn.strdisplaywidth(suffix)
+				 local targetWidth = width - sufWidth
+				 local curWidth = 0
+				 for _, chunk in ipairs(virtText) do
+					  local chunkText = chunk[1]
+					  local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+					  if targetWidth > curWidth + chunkWidth then
+							table.insert(newVirtText, chunk)
+					  else
+							chunkText = truncate(chunkText, targetWidth - curWidth)
+							local hlGroup = chunk[2]
+							table.insert(newVirtText, {chunkText, hlGroup})
+							chunkWidth = vim.fn.strdisplaywidth(chunkText)
+							-- str width returned from truncate() may less than 2nd argument, need padding
+							if curWidth + chunkWidth < targetWidth then
+								 suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+							end
+							break
+					  end
+					  curWidth = curWidth + chunkWidth
+				 end
+				 table.insert(newVirtText, {suffix, 'MoreMsg'})
+				 return newVirtText
+			end
+
+			-- Only depend on `nvim-treesitter/queries/filetype/folds.scm`,
+			-- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
+			require('ufo').setup({
+				 provider_selector = function(bufnr, filetype, buftype)
+					  return {'treesitter', 'indent'}
+				 end,
+				fold_virt_text_handler = handler
+			})
+		end
+	},
+	{
+		'luukvbaal/statuscol.nvim', -- customize the status column to remove the fold depth count
+		config = function()
+			local builtin = require('statuscol.builtin')
+			require('statuscol').setup(
+			  {
+				 relculright = true,
+				 segments = {
+					{text = {'%s'}, click = 'v:lua.ScSa'},
+					{text = {builtin.foldfunc}, click = 'v:lua.ScFa'},
+					{text = {builtin.lnumfunc, ' '}, click = 'v:lua.ScLa'}
+				 }
+			  }
+			)
+		end
+	},
+
 	'sophacles/vim-processing', -- make art not war
 })
