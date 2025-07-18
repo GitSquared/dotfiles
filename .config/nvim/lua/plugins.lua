98ac86c03c903989aa341073d41ab92989a29803
Gabriel Saillard <gabriel@saillard.dev>
2025-07-18 16:13:06 +0200

    nvim: fix sql.jnja detection

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -175,7 +175,28 @@ return require('lazy').setup({
 		end
 	},
 
-	'HiPhish/jinja.vim', -- Jinja template syntax support
+	{
+		'HiPhish/jinja.vim', -- Jinja template syntax support
+		config = function()
+			-- Configure filetype detection for .sql.jinja files
+			vim.filetype.add({
+				extension = {
+					['sql.jinja'] = 'sql.jinja',
+				},
+				pattern = {
+					['.*%.sql%.jinja'] = 'sql.jinja',
+				},
+			})
+
+			-- Additional autocmd to ensure proper filetype detection
+			vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+				pattern = "*.sql.jinja",
+				callback = function()
+					vim.bo.filetype = "sql.jinja"
+				end,
+			})
+		end
+	},
 
 	{
 		"stevearc/conform.nvim", -- Foramt code while preserving marks and folds
