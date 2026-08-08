504ffa20d3939dfee2b32dab2f1d247dc66fad8d
Gabriel Saillard <gabriel@saillard.dev>
2026-08-08 21:49:44 +0200

    updates

--- .config/fish/functions/full_upgrade.fish
+++ .config/fish/functions/full_upgrade.fish
@@ -3,6 +3,7 @@ function full_upgrade
 
     mprocs \
         "npm update -g" \
+        "npx skills update -g" \
         "fish -c 'fisher update'" \
         "fish -c 'fish_update_completions'" \
         "nvim --headless -c 'Lazy! sync' +qa" \
